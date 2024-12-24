1786d0b45117bcd8a344aec59e38200c313c8f2b
Vivek kushwah <vv100188@gmail.com>
2024-12-24 13:21:00 +0530

    second

--- loops.js
+++ loops.js
@@ -1,3 +1,52 @@
 // LOOPS AND STRINGS:-
 
-// 
+//(1) for loop:- Syntax- for(initiolisation; condition; updation){loop logic}
+
+// for(let i=1; i<=10; i++){
+//     console.log("Vivek")
+// }
+
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+// }
+
+// for (let a=5; a>0;a=a-1){
+//     console.log(a)
+// }
+
+// for(let b=1;b<=6;b++){
+//     if(b==4){
+//         break;//break get exit the code from that line 
+//     }
+//     else{
+//         console.log(b)
+//     }
+// }
+
+for(let j=1; j<=5;j++){
+    if(j==3){
+        continue;//(continue get the compilier to the updation process)
+    }
+    else{
+        console.log(j)
+    }
+}
+
+// (2) while loop:- Syntax let a=1(initiolisation)
+    // while(condition){
+//     updation;
+// }
+
+let w=1;
+
+while(w<=10){
+    console.log("vk");
+    w++;
+}
+
+let y=10;
+
+while(y<=10){
+    console.log("herry")
+    y--;
+}
